dc2742e61c56fb346cfd3ff93cec0f6e93b47ca2
Predrag Jevtic <predragjevtic@gmail.com>
2017-10-12 14:03:32 +0200

    Successfully authenticated with Firebase & Facebook

--- Social/Controller/SingInVC.swift
+++ Social/Controller/SingInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SingInVC: UIViewController {
 
@@ -20,6 +23,39 @@ class SingInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Pec: Unable to authenticate with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("Pec: User cancelled Facebook authentication")
+            } else {
+                print("JESS: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("JESS: Successfully authenticated with Firebase")
+//                if let user = user {
+//                    let userData = ["provider": credential.provider]
+//                    self.completeSignIn(id: user.uid, userData: userData)
+//                }
+            }
+        })
+    }
+    @IBAction func presed(_ sender: Any) {
+        print("Pressed")
+    }
+    
 }
-
---
+++ Social/Util/Costants.swift
@@ -0,0 +1,12 @@
+//
+//  Costants.swift
+//  social
+//
+//  Created by Predrag Jevtic on 10/11/17.
+//  Copyright © 2017 Predrag Jevtic. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
